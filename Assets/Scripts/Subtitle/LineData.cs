e4886fa723972d94be2640928532df323d717284
Vitalii Kovalenko PC <vitkovolenko@gmail.com>
2026-01-04 02:01:34 +0000

    Subtitle system added
    Just a rough sketch, very basic functionality

---
+++ Assets/Scripts/Subtitle/LineData.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialogue/Line Data", fileName = "Line")]
+
+// Data class represents replica for subtitles
+// Simply it is (what to say)
+public class LineData : ScriptableObject
+{
+    // --- Should make it automatic
+    public string id;
+
+    [TextArea(2, 6)]
+    public string text;
+    public AudioClip voice;
+
+    [Header("Timing (if no voice)")]
+    public float duration = 1.2f;
+    
+}
